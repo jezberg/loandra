2c60d412f0c4fda6cffcec4b3c378426083ca39d
Ole Lübke <ole.luebke@tuhh.de>
2025-02-07 17:36:15 +0200

    Set BouMS to verbose logging

--- solvers/glucose4.1/mtl/template.mk
+++ solvers/glucose4.1/mtl/template.mk
@@ -14,7 +14,7 @@ CHDRS      = $(wildcard $(PWD)/*.h)
 COBJS      = $(CSRCS:.cc=.o) $(DSRCS:.cc=.o)
 PREOBJ	   = $(wildcard $(PREPRO_DIR)/src/lib/*.a) 
 DPWOBJ	   = $(wildcard $(DPW_DIR)/target/release/*.a) 
-BOUMSRELOBJ = $(BOUMS_DIR)/build/release-nologging/libBouMS.a
+BOUMSRELOBJ = $(BOUMS_DIR)/build/release-logverbose/libBouMS.a
 BOUMSDBGOBJ = $(BOUMS_DIR)/build/debug-logverbose/libBouMS.a
 
 PCOBJS     = $(addsuffix p,  $(COBJS))
@@ -116,8 +116,8 @@ builddeps:
 	@echo Making RustSAT
 	cd $(DPW_DIR)/capi && cargo build --release
 	if [ $(BOUMSOBJ) = $(BOUMSRELOBJ) ]; then \
-		@echo "Making BouMS (release-nologging)"; \
-		$(MAKE) -C $(BOUMS_DIR) libonly-release-nologging; \
+		@echo "Making BouMS (release-logverbose)"; \
+		$(MAKE) -C $(BOUMS_DIR) libonly-release-logverbose; \
 	else \
 		@echo "Making BouMS (debug-logverbose)"; \
 		$(MAKE) -C $(BOUMS_DIR) libonly-debug-logverbose; \
